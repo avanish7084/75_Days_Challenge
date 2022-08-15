d88f76a7cd8e235e3efe83c6fce237014123369b
Avanish Dubey <73096949+avanish7084@users.noreply.github.com>
2022-08-15 11:04:00 +0530

    Update happyNumber.py

--- day1/happyNumber.py
+++ day1/happyNumber.py
@@ -1,1 +1,13 @@
 
+res, summy = n, 0
+dummy = [n]
+while res != 1:
+     s = str(res)
+     for i in range(len(s)):
+          summy += (int(s[i]) ** 2)
+     res = summy
+     summy = 0
+     if res in dummy:
+          eturn False
+     dummy.append(res)
+return True
