5497169f60a05f0a458ee8976fcf453ebd4453f3
Avanish Dubey <73096949+avanish7084@users.noreply.github.com>
2022-08-17 01:14:59 +0530

    Update Longest_Common_Prefix.py

--- day2/Longest_Common_Prefix.py
+++ day2/Longest_Common_Prefix.py
@@ -1,1 +1,22 @@
-
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        def lcs(s1,s2,n):
+            res=""
+            i=0
+            while n>0:
+                if s1[i]==s2[i]:
+                    res+=s1[i]
+                    i+=1
+                else:
+                    break
+                n-=1
+            
+            return res
+            
+        res=strs[0]
+        for i in range(1,len(strs)):
+            res=lcs(res,strs[i],min(len(res),len(strs[i])))
+        return res
+        
+        
+        
