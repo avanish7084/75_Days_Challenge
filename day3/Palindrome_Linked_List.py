8175df0d1266452c12dde0c3523de14d844996d7
Avanish Dubey <73096949+avanish7084@users.noreply.github.com>
2022-08-23 22:29:00 +0530

    Update Palindrome_Linked_List.py

--- day3/Palindrome_Linked_List.py
+++ day3/Palindrome_Linked_List.py
@@ -1,1 +1,56 @@
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        
+        # Method 2
+        
+        slow=fast=head
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow.next
+            
+        # reverse  second half node    
+        prev=nextp=None
+        while slow:
+            nextp=slow.next
+            slow.next=prev
+            prev=slow
+            slow=nextp
+        new_second_half_reverse_node=prev
+        
+        while new_second_half_reverse_node:
+            if new_second_half_reverse_node.val!=head.val:
+                return False
+            new_second_half_reverse_node=new_second_half_reverse_node.next
+            head=head.next
+        return True
+        
+        # Method 1
+        
+        
+        curr=head
+        res=head
+        prev=nextp=None
+        arr=[]
+        while res:
+            arr.append(res.val)
+            res=res.next
+        while curr:
+            nextp=curr.next
+            curr.next=prev
+            prev=curr
+            curr=nextp
+        
+        i=0
+        while prev: 
+            if arr[i]!=prev.val:
+                return False
+            i+=1
+            prev=prev.next
+        return True
+        
